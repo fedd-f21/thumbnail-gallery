3a458b077eaed7a66532438e00711883a754bc8c
Rocco P <roccopan@gmail.com>
2021-12-02 14:29:48 -0500

    complete

--- js/script.js
+++ js/script.js
@@ -1,27 +1,16 @@
 const lg = document.querySelector(`#lrgimg`)
-const t1 = document.querySelector(`#thumb1`)
-const t2 = document.querySelector(`#thumb2`)
-const t3 = document.querySelector(`#thumb3`)
 
+// Collect all .thumbnail <img> together in an Array (NodeList)
+const thumbs = document.querySelectorAll(`.thumbnail`)
 
 // Get the url string from the target, assigns it to the src of the large image
 const handleThumbClick = function(event) {
-  // Element that was clicked
-  const thumb = event.target
-
-  // The src url from the element
-  const url = thumb.getAttribute(`src`)
-
-  // Assign the string to the large image
-  lg.setAttribute(`src`, url)
-
-
   // The same as the above 3 lines, as one line of code
-  // lg.setAttribute(`src`, event.target.getAttribute(`src`))
+  lg.setAttribute(`src`, event.target.getAttribute(`src`))
 }
 
-
-// Add listeners to each thumbnail one by one
-t1.addEventListener(`click`, handleThumbClick)
-t2.addEventListener(`click`, handleThumbClick)
-t3.addEventListener(`click`, handleThumbClick)
+// LOOP: Add listeners to each thumbnail one by one
+thumbs.forEach(function(thumb) {
+  // For each of the thumbnails found, make it clickable
+  thumb.addEventListener(`click`, handleThumbClick)
+})
