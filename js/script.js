112d55cfd2227de5289fcbb91d0a1a4084a18fa5
Rocco P <roccopan@gmail.com>
2021-12-02 12:52:19 -0500

    Do al lthe work inside of a helper function

--- js/script.js
+++ js/script.js
@@ -4,22 +4,20 @@ const t2 = document.querySelector(`#thumb2`)
 const t3 = document.querySelector(`#thumb3`)
 
 // Recieves a url string, assigns it to the src of the large image
-const setLargeImgSrc = function(url) {
+const setLargeImgSrc = function(thumb) {
+  const url = thumb.getAttribute(`src`)
   lg.setAttribute(`src`, url)
 }
 
-
+// Add listeners to each thumbnail one by one
 t1.addEventListener(`click`, function() {
-  const url = t1.getAttribute(`src`)
-  setLargeImgSrc(url)
+  setLargeImgSrc(t1)
 })
 
 t2.addEventListener(`click`, function() {
-  const url = t2.getAttribute(`src`)
-  setLargeImgSrc(url)
+  setLargeImgSrc(t2)
 })
 
 t3.addEventListener(`click`, function() {
-  const url = t3.getAttribute(`src`)
-  setLargeImgSrc(url)
+  setLargeImgSrc(t3)
 })
