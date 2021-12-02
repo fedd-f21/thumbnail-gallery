0107af69079ea22cdf24892887cc43d4f4f03b01
Rocco P <roccopan@gmail.com>
2021-12-02 12:49:05 -0500

    Function to assign string url

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@ const t1 = document.querySelector(`#thumb1`)
 const t2 = document.querySelector(`#thumb2`)
 const t3 = document.querySelector(`#thumb3`)
 
+// Recieves a url string, assigns it to the src of the large image
+const setLargeImgSrc = function(url) {
+  lg.setAttribute(`src`, url)
+}
+
+
 t1.addEventListener(`click`, function() {
   const url = t1.getAttribute(`src`)
-  lg.setAttribute(`src`, url)
+  setLargeImgSrc(url)
 })
 
 t2.addEventListener(`click`, function() {
   const url = t2.getAttribute(`src`)
-  lg.setAttribute(`src`, url)
+  setLargeImgSrc(url)
 })
 
 t3.addEventListener(`click`, function() {
   const url = t3.getAttribute(`src`)
-  lg.setAttribute(`src`, url)
+  setLargeImgSrc(url)
 })
